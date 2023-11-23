e2911dab09bb7ce0206c1718ec03119b9b7b29e1
MarcosSanPedro <143010406+MarcosSanPedro@users.noreply.github.com>
2023-11-23 01:37:19 +0000

    sad

---
+++ src/components/Cart.jsx
@@ -0,0 +1,22 @@
+export async function loader(){
+    try{
+    const response = await fetch(
+        "https://random-data-api.com/api/v2/users?size=10"
+    );
+    const data = await response.json();
+    console.log(data);
+}catch (error){
+    console.error("error fetching",error);
+    return[]
+}}
+
+export const Cart = ()=> {
+
+
+    return(
+        <ul>
+            <link to="/user">
+        </ul>
+    )
+
+}
